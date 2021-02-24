2b9208d168e8882227e9ccd278467fe5fba2c928
shinepreventer <13852983275@163.com>
2021-02-24 16:36:20 +0800

    Default Changelist

---
+++ BMI.py
@@ -0,0 +1,14 @@
+print("请输入身高（米）")
+height = input()
+print("请输入体重（千克）")
+weight = input()
+BMI = float(weight) / (float(height) * float(height))
+print("您的体重指数为: %r," % (BMI))
+if BMI <= 18:
+    print("体重过低")
+elif BMI > 18 and BMI < 24:
+    print("体重正常")
+elif BMI >= 24 and BMI < 28:
+    print("超重")
+else:
+    print("肥胖")
